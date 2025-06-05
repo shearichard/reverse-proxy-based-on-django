59146e52478e95e7661fe9e4c3e94233f43bf280
Richard Shea <rshea@thecubagroup.com>
2025-06-05 22:22:44 +1200

    Create new 'users' app, with customer user which uses email address in lieu of userid.
    Make corresponding changes to admin, etc.
    
    Also do initial migrations including the customer user.

--- proxy_app/core/settings.py
+++ proxy_app/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Specify the custom user class for the purposes of authorisation
+AUTH_USER_MODEL = "users.CustomUser"
+
